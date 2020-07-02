79e9c63594c75e4de8120ba4d264a7e3e1242e7b
ryancvaughn <67763139+ryancvaughn@users.noreply.github.com>
2020-07-02 15:07:30 -0700

    i added horizontal collision and part of the vertical collision.

--- stickman/stickmangame.py
+++ stickman/stickmangame.py
@@ -2,6 +2,28 @@ from tkinter import *
 import random
 import time
 
+class Coords:
+    def __init__(self, x1=0, y1=0, x2=0, y2=0):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+def within_x(co1, co2):
+    if co1.x1 > co2.x1 and co1.x1 < co2.x2:
+        return True
+    elif co1.x2 > co2.x1 and co1.x2 < co2.x2:
+        return True
+    elif co2.x1 > co1.x1 and co2.x1 < co2.x2:
+        return True
+    elif co2.x2 > co1.x1 and co2.x2 < co1.x2:
+        return True
+    else:
+        return False
+def within_y(co1, co2):
+    if (co1.y1 > co2.y1 and co1.y1 < co2.y2):
+            or (co1.y2 > co2.y1 and co1.y2 < co2.y2)
+            or (co2.y1 > co1.y1 and co2.y1 < co2.y2)
+        
 class Game:
     def __init__(self):
         self.tk = Tk()
